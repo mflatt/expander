88ef82ba8e2e1bfac5a9e012b1a8bffb46e7323f
Matthew Flatt <mflatt@racket-lang.org>
2016-05-11 07:45:25 -0600

    fix matcher for 0 matches on `...`

--- match.rkt
+++ match.rkt
@@ -23,14 +23,14 @@
                (eq? '...+ (cadr pattern))))
       (define flat-s (to-syntax-list s))
       (cond
-       [(null? flat-s) null]
+       [(null? flat-s)
+        (when (eq? '...+ (cadr pattern))
+          (error "bad syntax:" orig-s))
+        (make-empty-vars pattern)]
        [(list? flat-s)
         (define a-lists
           (for/list ([s (in-list flat-s)])
             (match s (car pattern))))
-        (when (and (eq? '...+ (cadr pattern))
-                   (null? (car a-lists)))
-          (error "bad syntax:" orig-s))
         (apply map
                (lambda slice
                  (list (caar slice)
@@ -47,7 +47,8 @@
       (cond
        [(null? s) null]
        [else (error "bad syntax:" orig-s)])]
-     [(and (keyword? pattern)
+     [(and (or (keyword? pattern)
+               (boolean? pattern))
            (eq? pattern s))
       null]
      [else
@@ -57,8 +58,24 @@
     (define a (assq sym a-list))
     (if a
         (cadr a)
-        ;; assume a sequence with 0 matches
-        null)))
+        (error "no such pattern variable:" sym))))
+
+(define (make-empty-vars pattern)
+  (cond
+   [(symbol? pattern)
+    (list (list pattern null))]
+   [(and (list? pattern)
+         (= (length pattern) 2)
+         (or (eq? '... (cadr pattern))
+             (eq? '...+ (cadr pattern))))
+    (map (lambda (m)
+           (cons (car m) (list (cadr m))))
+         (make-empty-vars (car pattern)))]
+   [(pair? pattern)
+    (append (make-empty-vars(car pattern))
+            (make-empty-vars(cdr pattern)))]
+   [else
+    null]))
 
 (define (try-match-syntax orig-s pattern)
   (let/ec esc
